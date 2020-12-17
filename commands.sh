77bce869370a21e297807b376c19d8c84f3cd0de
Tom Kuipers <t.b.kuipers@outlook.com>
2020-12-17 14:57:33 +0100

    Update

--- commands.sh
+++ commands.sh
@@ -2,8 +2,17 @@
 ## pip install -U cookiecutter
     # cookiecutter https://github.com/audreyfeldroy/cookiecutter-pypackage.git
 
+## Install during testing
+    # pip install -e Documents/CodeProjects/Python/predex/
+
 ## Upload to pypi (navigate to right dir)
     # python setup.py sdist
     # twine upload dist/*
 
-## pip install -e Documents/CodeProjects/Python/shiny-py/
+## Conda package
+    # conda skeleton pypi predex
+    # conda-build predex
+
+## Install
+    # conda install --use-local /home/tbkuipers/miniconda3/envs/test/conda-bld/linux-64/predex-0.1.1-py37_0.tar.bz2
+    # conda install --use-local predex
